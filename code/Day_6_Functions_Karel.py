392d370b5ed41ad32763bbab8ff2e5b62da14ac0
PushCraft <consultant.jaskaran@gmail.com>
2025-09-14 17:37:51 -0400

    J

---
+++ code/Day_6_Functions_Karel.py
@@ -0,0 +1,35 @@
+
+
+# Inbuilt functions, Example 'print()', 'len()'
+print("Hello!")
+len=len("8729347")
+print(len)
+
+# Practise using this link:https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+# Custom functions : Define your own functions.
+def functionNameX():
+    print("Hello, Code from functionNameX")
+    print("Good Bye!!")                # Indendetions are very important. 
+
+
+# to Call/Trigger the function.
+functionNameX()
+
+
+
+
+# Loops : 
+
+# Do While:
+
+#  while something_is_true:
+      # Action
+
+index = 1
+while 2 == 1:
+    print("Keep printing this...")
+    index = index +  1
+    print(index) 
+    
+
